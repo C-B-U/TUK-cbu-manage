21d38f2a394bfed51938ffb26a1233bff731ae4a
ming0o <isha3992@gmail.com>
2025-01-11 00:05:54 +0900

    refactor: 헤더/푸터 파일 수정

--- frontend/src/components/shared/Footer.vue
+++ frontend/src/components/shared/Footer.vue
@@ -1,71 +1,107 @@
-<script setup lang="ts"></script>
-
 <template>
     <footer>
         <router-link to="/">
-            <img id="footer-logo" src="../../assets/logo.png" alt="로고이미지">
+            <img id="footer-logo" src="@/assets/logo.png" alt="로고이미지" />
         </router-link>
-            
+
         <div class="footer-info-area">
-            <p class="footer-info">회장 : 정우혁 ㅣ이메일 : 000@tukorea.ac.kr ㅣ 번호 : 010-1234-5678</p>
-            <p class="footer-info">부회장 : 정우혁 ㅣ이메일 : 000@tukorea.ac.kr ㅣ 번호 : 010-1234-5678</p>
+            <p class="footer-info" v-for="(info, index) in footerInfo" :key="index">
+                {{ info.title }}: {{ info.name }} ㅣ 이메일: {{ info.email }} ㅣ 번호: {{ info.phone }}
+            </p>
         </div>
 
         <div class="social-area">
-            <a href="#">인스타그램</a>
-            <a href="#">한국공학대학교</a>
+            <a v-for="(link, index) in socialLinks" :key="index" :href="link.url">
+                <img v-if="link.icon" :src="link.icon" alt="인스타그램" class="social-icon" />
+            </a>
         </div>
 
-        <hr>
-        <p class="copyright">Copyright ⓒ 2024 씨부엉 All rights reserved.</p>
-
+        <hr />
+        <p class="copyright">
+            Copyright ⓒ 2025 씨부엉 All rights reserved.
+        </p>
     </footer>
 </template>
+
+<script setup>
+import instagramIcon from '@/assets/instagram.svg';
+import { ref } from 'vue';
+
+// 연락처 정보 데이터
+const footerInfo = [
+    {
+        title: '회장',
+        name: '정우혁',
+        email: '000@tukorea.ac.kr',
+        phone: '010-1234-5678',
+    },
+    {
+        title: '부회장',
+        name: '정우혁',
+        email: '000@tukorea.ac.kr',
+        phone: '010-1234-5678',
+    },
+];
+
+// 소셜 링크 데이터
+const socialLinks = [
+    { name: '인스타그램', url: 'https://www.instagram.com/tukorea_cbu', icon: instagramIcon },
+    { name: '한국공학대학교', url: '#' },
+];
+</script>
 
 <style scoped>
 footer {
     width: 100%;
     padding: 32px var(--entirePadding);
-
-    background-color: #EEEEEE;
+    background-color: #eeeeee;
 }
 
-    #footer-logo {
-        width: 132px;
-        height: auto;
-        mix-blend-mode: darken;
-    }
+#footer-logo {
+    width: 132px;
+    height: auto;
+    mix-blend-mode: darken;
+}
 
 footer .footer-info-area {
-    margin: 32px 0px;
+    margin: 32px 0;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
 }
 
-    .footer-info-area p {
-        text-align: center;
-        font-size: 14px;
-        color: #333;
-    }
+.footer-info-area p {
+    text-align: center;
+    font-size: 14px;
+    color: #333;
+}
 
 .social-area {
     display: flex;
-    gap:16px;
+    gap: 16px;
     justify-content: center;
 }
 
-    .social-area a {
-        font-size: 14px;
-        color:#333;
-    }
+.social-area a {
+    font-size: 14px;
+    color: #333;
+}
+
+.social-icon {
+    width: 25px;
+    height: 25px;
+    display: inline-block;
+}
 
 footer hr {
-    border:none;
-    height:1px;
-    background-color: #CCCCCC;
-    margin:12px 0px;
+    border: none;
+    height: 1px;
+    background-color: #cccccc;
+    margin: 12px 0;
 }
 
 .copyright {
     font-size: 12px;
-    color:#888;
+    color: #888;
 }
 </style>
--- frontend/src/components/shared/Header.vue
+++ frontend/src/components/shared/Header.vue
@@ -1,31 +1,46 @@
-<script setup lang="ts"></script>
-
 <template>
     <header>
         <div id="header-nav">
             <router-link to="/">
-                <img id="logo" src="../../assets/logo.png" alt="로고이미지">
+                <img id="logo" src="@/assets/logo.png" alt="로고이미지" />
             </router-link>
-            
+
             <nav>
                 <ul>
-                    <li>
-                        <router-link to="/manage-M">회원 관리</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/manage-SA">합격자 관리</router-link>
+                    <li v-for="item in navItems" :key="item.path">
+                        <router-link :to="item.path">{{ item.name }}</router-link>
                     </li>
                 </ul>
             </nav>
         </div>
 
         <div id="util">
-            <router-link to="/login">로그인</router-link>
-            <router-link to="/join">회원가입</router-link>
+            <ul>
+                <li v-for="item in utilItems" :key="item.path">
+                    <router-link :to="item.path">{{ item.name }}</router-link>
+                </li>
+            </ul>
         </div>
-
     </header>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+// 네비게이션 항목 정의
+const navItems = [
+    { name: '회원 관리', path: '/manage-M' },
+    { name: '합격자 관리', path: '/manage-SA' },
+];
+
+const utilItems = [
+    { name: '로그인', path: '/login' },
+    { name: '회원가입', path: '/join' },
+];
+</script>
 
 <style scoped>
 header {
@@ -33,18 +48,17 @@ header {
     display: flex;
     justify-content: space-between;
     align-items: center;
-
     padding: 20px var(--entirePadding);
 }
 
 header a {
     display: flex;
-    color:#333;
+    color: #333;
     transition: all 0.3s;
 }
 
 header a:hover {
-    color:var(--mainColor);
+    color: var(--mainColor);
 }
 
 #header-nav {
@@ -53,28 +67,33 @@ header a:hover {
     gap: 200px;
 }
 
-    #header-nav #logo {
-        width: 132px;
-        height: auto;
-    }
+#header-nav #logo {
+    width: 132px;
+    height: auto;
+}
 
-    #header-nav nav ul {
-        display: flex;
-        gap: 120px;
-    }
+#header-nav nav ul {
+    display: flex;
+    gap: 120px;
+}
 
-    #header-nav nav ul li a {
-        font-size: 16px;
-        font-weight: 600;
-    }
+#header-nav nav ul li a {
+    font-size: 16px;
+    font-weight: 600;
+}
 
 #util {
     display: flex;
-    gap:24px;
+    gap: 24px;
 }
 
-    #util a {
-        font-size: 14px;
-        font-weight: 400;
-    }
+#util ul {
+    display: flex;
+    gap: 24px;
+}
+
+#util a {
+    font-size: 14px;
+    font-weight: 400;
+}
 </style>
