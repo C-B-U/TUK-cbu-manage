f75cfe31c0c937fcbd735cfa306f3cb7645c203d
SH4316 <soohyuk4316@gmail.com>
2025-01-30 18:44:09 +0900

    feat: login

---
+++ backend/src/main/java/com/example/cbumanage/authentication/entity/LoginEntity.java
@@ -0,0 +1,32 @@
+package com.example.cbumanage.authentication.entity;
+
+import com.example.cbumanage.authentication.authorization.Permission;
+import com.example.cbumanage.authentication.entity.converter.PermissionConverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	@Column(unique = true, nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	@Column(name = "permissions")
+	@Convert(converter = PermissionConverter.class)
+	private List<Permission> permissions;
+
+	public LoginEntity(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+}
