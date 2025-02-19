e04e6254af98b84cff83d1e3466b5e37e79a69f9
SeongEun <bongfam2@naver.com>
2025-02-19 19:15:29 +0900

    fix: 로그인 시 이름 반환

--- backend/src/main/java/com/example/cbumanage/authentication/controller/LoginController.java
+++ backend/src/main/java/com/example/cbumanage/authentication/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.example.cbumanage.authentication.exceptions.InvalidJwtException;
 import com.example.cbumanage.authentication.intercepter.AuthenticationInterceptor;
 import com.example.cbumanage.authentication.service.LoginService;
 import com.example.cbumanage.dto.MemberCreateDTO;
+import com.example.cbumanage.model.CbuMember;
 import com.example.cbumanage.model.SuccessCandidate;
 import com.example.cbumanage.model.enums.Role;
 import com.example.cbumanage.repository.CbuMemberRepository;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * LoginController는 로그인, 회원가입, 비밀번호 변경, 회원 탈퇴와 같은 사용자 인증 관련 엔드포인트를 제공하는 REST 컨트롤러입니다.
@@ -70,12 +72,13 @@ public class LoginController {
 	 * 헤더에 이메일과 비밀번호를 포함하여 요청합니다.
 	 *
 	 * @param password 로그인 비밀번호
-	 * @param res HttpServletResponse를 사용하여 쿠키를 추가합니다.
+	 * @param res      HttpServletResponse를 사용하여 쿠키를 추가합니다.
+	 * @return
 	 */
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
 	@Operation(summary = "로그인 후 쿠키에 토큰 반환", description = "헤더에 학번과 비밀번호를 넣어 요청")
-	public void login(
+	public Optional<CbuMember> login(
 			@RequestHeader(name = "studentNumber") final Long studentNumber,
 			@RequestHeader(name = "password") final String password,
 			HttpServletResponse res) {
@@ -88,6 +91,8 @@ public class LoginController {
 		for (Cookie cookie : cookies) {
 			res.addCookie(cookie);
 		}
+		Optional<CbuMember> cbuMember = cbuMemberRepository.findNameByStudentNumber(studentNumber);
+		return cbuMember;
 	}
 
 	/**
