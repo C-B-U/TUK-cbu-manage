823beb5a003b52faea5d6ee74a0b7bbc40c8196e
SeongEun <bongfam2@naver.com>
2025-02-28 18:15:42 +0900

    fix: 로그인 시 반환 값 변경

--- backend/src/main/java/com/example/cbumanage/authentication/repository/LoginRepository.java
+++ backend/src/main/java/com/example/cbumanage/authentication/repository/LoginRepository.java
@@ -2,6 +2,7 @@ package com.example.cbumanage.authentication.repository;
 
 import com.example.cbumanage.authentication.entity.LoginEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,8 @@ public interface LoginRepository extends JpaRepository<LoginEntity, Long> {
 	Optional<LoginEntity> findByEmailEquals(String email);
 
 	Optional<LoginEntity> findBystudentNumber(Long studentNumber);
+
+	@Query("select siteMember.email from LoginEntity siteMember WHERE siteMember.studentNumber = :studentNumber")
+	String findEmailBystudentNumber(Long studentNumber);
+
 }
