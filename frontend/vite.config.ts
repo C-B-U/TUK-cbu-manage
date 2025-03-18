e24cc096fe847909b8427992a9e47701dc8bd77b
ming0o <isha3992@gmail.com>
2025-03-18 15:14:22 +0900

    fix: css 파일 누락 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  vuetify({ autoImport: true }),
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true }),
   ],
   resolve: {
     alias: {
-      '@': '/src', // 경로 별칭 설정
+      "@": path.resolve(__dirname, "src"),
     },
   },
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/main.scss";`,
+      },
+    },
+  },
+  base: "./",
+});
