de51692bb1bcaadc846532e6f8e6bd40f8977a6f
ming0o <isha3992@gmail.com>
2025-06-28 20:07:56 +0900

    초기 설정 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,34 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import path from "path";
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use 'vuetify/settings' with (
-        $body-font-family: ('Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif),
-        $font-size-root: 1rem
-      );`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      vue(),
+      vuetify({ autoImport: true }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-  },
-  base: '/',
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use 'vuetify/settings' with (
+          $body-font-family: ('Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif),
+          $font-size-root: 1rem
+        );`,
+        },
+      },
+    },
+    base: '/',
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+    },
+  };
 });
